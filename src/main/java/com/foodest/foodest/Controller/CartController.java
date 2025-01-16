6c039271f137e57669195b69157bb184ad9e5761
Michel <michelcufre3002@gmail.com>
2025-01-16 06:33:43 -0300

    Operaciones Cart Controller

--- src/main/java/com/foodest/foodest/Controller/CartController.java
+++ src/main/java/com/foodest/foodest/Controller/CartController.java
@@ -1,5 +1,7 @@
 package com.foodest.foodest.Controller;
 import com.foodest.foodest.Classes.Cart;
+import com.foodest.foodest.Classes.Client;
+import com.foodest.foodest.Classes.Product;
 import com.foodest.foodest.Repository.CartRepository;
 import com.foodest.foodest.Services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
     @Autowired
     private CartService cartService;
+
+    @GetMapping("/{email}")
+    public ResponseEntity<Cart> getCartByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(cartService.getCartByEmail(email));
+    }
+
+    @PostMapping("/{email}/update")
+    public ResponseEntity<String> updateCart(
+            @PathVariable String email,
+            @RequestBody List<Product> products) {
+        cartService.updateCart(email, products);
+        return ResponseEntity.ok("Cart updated successfully!");
+    }
+
+    @GetMapping("/{email}/empty")
+    public ResponseEntity<String> emptyCart(@PathVariable String email) {
+        cartService.emptyCart(email);
+        return ResponseEntity.ok("Client deleted successfully!");
+    }
 }
